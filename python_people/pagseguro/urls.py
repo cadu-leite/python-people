114b5c3661ae0a3baa600dd23c146de15bacb143
Carlos E. C. Leite <caduado@gmail.com>
2012-01-31 02:14:22 -0200

    alterado import urls

--- python_people/pagseguro/urls.py
+++ python_people/pagseguro/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^checkout/$', 'payment.views.checkout', name='pay-checkout'),
-    url(r'^checkout_return/$', 'payment.views.checkout_return', name='pay-checkout_return'),
+    url(r'^checkout/$', 'pagseguro.views.checkout', name='pay-checkout'),
+    url(r'^checkout_return/$', 'pagseguro.views.checkout_return', name='pay-checkout_return'),
      
 )
