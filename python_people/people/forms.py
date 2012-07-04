ce3a24ccd2b57bff3e31813bcd3bf86632a0abdd
Cadu Leite <carlos.leite@znc.com.br>
2012-07-04 19:18:35 -0300

    pep8

--- python_people/people/forms.py
+++ python_people/people/forms.py
@@ -4,18 +4,18 @@ from django.contrib.gis import forms
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.utils.translation import ugettext_lazy as _
-from people.models import SEXO_CHOICES, UserProfile, PythonFrameWorks, PythonGroup
+#from django.utils.translation import ugettext_lazy as _
+from people.models import UserProfile,  PythonGroup
 
 
-    
 class UserRegisterForm(UserCreationForm):
     username = forms.EmailField(label="e-mail", max_length=64, help_text="e-mail as username")
     forms.EmailField()
+
     class Meta:
         model = User
-        fields=('username', 'password1', 'password2')
-    
+        fields = ('username', 'password1', 'password2')
+
     def save(self, commit=True):
         user = super(UserRegisterForm, self).save(commit=False)
         user.email = self.cleaned_data["username"]
@@ -24,26 +24,26 @@ class UserRegisterForm(UserCreationForm):
             user.save()
         return user
 
-      
+
 class UserProfileForm(forms.ModelForm):
-    
+
     class Meta:
         model = UserProfile
         exclude = ('user')
-        widgets={
-                 'point':forms.HiddenInput(),
+        widgets = {
+                 'point': forms.HiddenInput(),
                  'python_frameworks': forms.CheckboxSelectMultiple(),
                  }
 
+
 class PythonGroupForm(forms.ModelForm):
-        
+
     class Meta:
         model = PythonGroup
         exclude = ('user')
-        widgets={
-                 'point':forms.HiddenInput(),
-                 'date_add':forms.HiddenInput(),
-                 'date_upd':forms.HiddenInput(),
+        widgets = {
+                 'point': forms.HiddenInput(),
+                 'date_add': forms.HiddenInput(),
+                 'date_upd': forms.HiddenInput(),
                  }
 
-   
