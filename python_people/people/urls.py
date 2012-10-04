6a70a66b210c1d60c577f5dd27b8eb3c1ea09ea2
Paulo Roberto Silva <paulo.pinda@gmail.com>
2012-10-04 17:04:19 -0300

    =]

--- python_people/people/urls.py
+++ python_people/people/urls.py
@@ -16,7 +16,7 @@ from people.views import (
 urlpatterns = patterns('',
     url(r'^kml/$', points, name='points'),
     #url(r'^profile/(?P<id>\d+)/$', user_profile_upd, name='userprofile-upd'),
-    #url(r'^profile/list/$', ListView.as_view(queryset=UserProfile.objects.order_by("name"), template_name='people/userprofile_list.html',paginate_by=15) , name="user-profile-list"),
+    ##url(r'^profile/list/$', ListView.as_view(queryset=UserProfile.objects.order_by("name"), template_name='people/userprofile_list.html',paginate_by=15) , name="user-profile-list"),
     url(r'^profile/list/$', profile_list, name="user-profile-list"),
     url(r'^profile/(?P<pk>\d+)/$', DetailView.as_view(model=UserProfile), name="user-profile"),
     url(r'^profile/form/$', user_profile_crud, name="user-profile-form"),
